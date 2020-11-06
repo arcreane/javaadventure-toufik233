9df4d882baf98252fea7aa9dd1252fd3c9ffe7d0
toufik <taoufik.elkasmi@gmail.com>
2020-11-06 11:08:31 +0100

    creation classe dongons

--- src/com/company/Aventurier.java
+++ src/com/company/Aventurier.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Aventurier {
-}
+
+    public static void main(String[] args) {
+            Random C = new Random();
+            int essai =0;
+
+            int justprix = C.nextInt(200);
+            int prix =0;
+            while (justprix !=prix){
+                System.out.println("entre un numero entre 0 et 200");
+                Scanner scan = new Scanner(System.in);
+                if( justprix > prix){
+                    System.out.println(" c'est plus ");
+                }else{
+                    System.out.println("c'est moins");
+                }
+                prix = scan.nextInt();
+
+                essai++;
+
+
+            }
+            System.out.println(" bravo : le just prix :" + justprix);
+            System.out.println(" en plus " + essai + " essai");
+        }
+    }
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,44 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new Aventurier();
+        System.out.println("Trésor");
+        Random C = new Random();
+        int essai = C.nextInt(10);
+        boolean batom1tf = false;
+        String batom1 = "batom orginal ";
+        // games
+        int bois = 0;
+        int attack = 1;
+        int life = 10;
+        int lifeplayer = 10;
+        int enemeylife = 15;
+        int fight = 0;
+        // points
+        int point = 0;
+        int point2 = 5;
+        int point3 = 10;
+        int pointmax = 3;
+
+        while (attack != lifeplayer) {
+            System.out.println("angelique");
+            Scanner scan = new Scanner(System.in);
+            if( life < attack ){
+                System.out.println(" tu es mort ");
+            }else{
+                System.out.println(" ");
+            }
+            pointmax= scan.nextInt();
+
+            essai++;
+
+
+        }
     }
 }
+
